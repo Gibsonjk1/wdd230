37aa7e27a4b9a6aa1308ba92896ea88d4b9b2743
Jason Gibson <jagibson@melaleuca.com>
2024-09-23 08:57:19 -0600

    course portal creation

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const d = new Date();
+let year = d.getFullYear();
+document.querySelector(
+  "#copyright"
+).innerHTML = `Copyright ${year} Jason K Gibson Idaho USA`;
+
+document.querySelector(
+  "#lastModified"
+).innerHTML = `Last Modified: ${document.lastModified}`;
