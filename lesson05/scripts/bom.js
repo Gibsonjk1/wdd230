e676f41ae272658579efda6a2b10c707dc7f9be9
Jason Gibson <jagibson@melaleuca.com>
2024-10-17 21:52:30 -0600

    fixes to BOM tool

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -1,9 +1,10 @@
-const input = document.querySelector("#favchap")
-const button = document.querySelector("button")
-const list = document.querySelector("#list")
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
 button.addEventListener('click', () => {
-    if (input.value !== null) {
+    if (input.value !== '') {
+
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
         li.innerHTML = input.value;
