80665b46a0345245a21a8787b76824ec2b4eb363
Jason Gibson <jagibson@melaleuca.com>
2024-10-17 21:45:09 -0600

    Chamber fixes and BOM DOM manipulation

--- chamber/scripts/hamburger.js
+++ chamber/scripts/hamburger.js
@@ -1,7 +1,8 @@
 const hamburger = document.querySelector('#hamburger');
-const nav = document.querySelector('.nav');
+const nav = document.querySelector('#nav');
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('open');
     nav.classList.toggle('open');
+    nav.classList.toggle('closed');
 })
